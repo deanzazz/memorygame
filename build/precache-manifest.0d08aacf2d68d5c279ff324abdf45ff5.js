0b4331976af2a86b97c64ea797be95b7afde999f
root <root@DESKTOP-24DC6OD>
2023-02-07 23:37:11 -0500

    first commit

---
+++ build/precache-manifest.0d08aacf2d68d5c279ff324abdf45ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44da11519458a368a90f",
+    "url": "/static/css/main.2ea92762.chunk.css"
+  },
+  {
+    "revision": "44da11519458a368a90f",
+    "url": "/static/js/main.44da1151.chunk.js"
+  },
+  {
+    "revision": "047dec1b64253260b63c",
+    "url": "/static/js/1.047dec1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b0328b5e595d27e0172396a5dc17b4f",
+    "url": "/index.html"
+  }
+];
